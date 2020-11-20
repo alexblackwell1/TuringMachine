ce9179adfbc719a617c046514e8304938fd0c335
alexblackwell1 <69765981+alexblackwell1@users.noreply.github.com>
2020-11-19 21:57:51 -0500

    Update Turning.hs

--- Turning.hs
+++ Turning.hs
@@ -16,15 +16,7 @@ data Transition    = (InTransition, OutTransition)
 -- where states DO start with q then # and the tape/alphabet cannot have characters that start with q or d/D
 -- where the tape alphabet includes the input alphabet and the blank symbol
 
---data Token = StateSet | TapeSet | AlphaSet | StartState | Blank | FinalSet
---  | KeywordState State | KeywordTape TapeA | KeywordAlpha Alpha
---  | Comma | Semi | LBracket | RBracket | LPar | RPar | Eql
---  | Err deriving Show
-
---or (thinking ^ is wrong)
-
-data Token = Machine | Delta
-  | KeywordState State | KeywordTape TapeA | Keyword String
+data Token = Delta | KwS State | KwT TapeA | Dir Direction
   | Comma | Semi | LBracket | RBracket | LPar | RPar | Eql
   | Err deriving Show
 
@@ -36,62 +28,53 @@ classify "{" = LBracket
 classify "}" = RBracket
 classify "," = Comma
 classify ";" = Semi
-classify x | isState x = KeywordState (toState x)
-classify x | isTape x = KeywordTape x --one of the last parts that needs to be defined
+classify ";" = Semi
+classify x | x == "d" || x == "D" = Delta
+classify x | isDirection x = Dir (toDirection x)
+classify x | isState x = KwS (toState x)
+classify x | isTapeA x = KwT x --one of the last parts that needs to be defined
 
 -- input 0 for current. It stands for the current part of the tuple being processed
 sr :: [Token] -> [State] -> [TapeA] -> [TapeA] -> [Transition] -> State -> TapeA -> [State] -> Integer -> ([State], [TapeA], [TapeA], [Transition], State, TapeA, [State])
 -- I don't think we'll need the transition stack
 -- sr [] (x:xs) states tape alpha trans start empty final current = sr [x] xs states tape alpha trans start empty final current
-sr (LPar : xs) l s t a tr st e f c  | c == 0 = sr xs l s t a tr st e f (c+1)
-                                    | --otherwise is a transition
-sr (RPar : xs) l s t a tr st e f c  | c == 7 = sr xs l s t a  tr st e f 4
-                                    | -- otherwise is a transition
-sr (LBracket : xs) l s t a tr st e f c = sr xs l s t a tr st e f c
-sr (RBracket : xs) l s t a tr st e f c = sr xs l s t a tr st e f c
-sr (KeywordState x : xs)  l s t a tr st e f c | c == 1 = sr xs l (x:s) t a tr st e f c
-                                              | c == 5 = sr xs l s t a tr x e f c
-                                              | otherwise = sr xs l s t a tr st e (x:f) c
-sr (KeywordTape x : xs)  l s t a tr st e f c | c == 2 = sr xs l s (x:t) a tr st e f c
-                                                 | c == 
-sr (Semi : xs) l s t a tr st e f c = sr xs l s t a  tr st e f (c+1)
-sr (Comma : xs) l s t a tr st e f c = sr xs l s t a tr st e f c
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-sr _ _ _ _ _ _ _ _ _ = 
-
---read
-  --the following variables will be instantiated
---states :: [State]
---  states   = ints between {} and before 1st ;
---alphabet :: [TapeA]
---  alphabet = char between {} and before 2nd ;
---tape :: [TapeA]
---  tape     = char between {} and before 3rd ;
---transitions :: [Transition]
---  transitions = transitions before 4th ;
---startingState :: State
---  startingState = state before 5th ;
---empty :: TapeA
---  empty = char before 6th ;
---accState :: [State]
---  accState = states between {} before 7th ;
-
-
+sr (LPar : xs) s t a tr st e f c  | c == 0 = sr xs s t a tr st e f (c+1)
+sr (RPar : xs) s t a tr st e f c  | c == 7 = sr xs s t a  tr st e f 4
+sr (LBracket : xs) s t a tr st e f c = sr xs s t a tr st e f c
+sr (RBracket : xs) s t a tr st e f c = sr xs s t a tr st e f c
+sr (Semi : xs) s t a tr st e f c = sr xs s t a  tr st e f (c+1)
+sr (Comma : xs) s t a tr st e f c = sr xs s t a tr st e f c
+sr (KeywordState x : xs) s t a tr st e f c | c == 1 = sr xs (x:s) t a tr st e f c
+                                           | c == 5 = sr xs s t a tr x e f c
+                                           | c == 7 = sr xs s t a tr st e (x:f) c
+sr (KeywordTape x : xs) s t a tr st e f c | c == 2 = sr xs s (x:t) a tr st e f c
+                                          | c == 3 = sr xs s t (x:a) tr st e f c
+                                          | c == 6 = sr xs s t a tr st a f c
+sr (Delta : LPar : KwS x1 : Comma : KwT x2 : RPar : Eql : KwS x3 : Comma : KwT x4 : Comma : Dir x5 : RPar : xs) s t a tr st e f c
+  = sr xs s t a (((x1, x2), (x3, x4, x5)):tr) st a f c
+sr [] s t a tr st e f _ = (s, t, a, tr, st, e, f)
 
 first :: a -> b
-first (x:y:z:s) = x
+first (x:xs) = x
 
 second :: a -> b
-second (x:y:z:s) = y
+second (x1:x2:xs) = x2
 
 third :: a -> b
-third (x:y:z:s) = z
+third (x1:x2:x3:xs) = x3
+
+fourth :: a -> b
+fourth (x1:x2:x3:x4:xs) = x4
+
+fifth :: a -> b
+fifth (x1:x2:x3:x4:x5:xs) = x5
+
+sixth :: a -> b
+sixth (x1:x2:x3:x4:x5:x6:xs) = x6
+
+seventh :: a -> b
+seventh (x1:x2:x3:x4:x5:x6:x7:xs) = x7
+
 
 isAlpha :: Char -> Bool
 isAlpha x = ('A' <= x && x <= 'Z') || ('a' <= x && x <= 'z')
@@ -114,12 +97,20 @@ toState (x:xs)    = (read x) : toState xs
 toState []        = []
 
 -- Anything that can be in the INPUT or TAPE alphabet
-isTape :: String -> Bool
-isTape "" = False
-isTape (x:xs) = (x /= 'q') && (x /= 'd')
+isTapeA :: String -> Bool
+isTapeA "" = False
+isTapeA (x:xs) = (x /= 'q') && (x /= 'd')
 
 isValid :: String -> Bool
 isValid x = x `elem` alphabet
+
+isDirection :: String -> Bool
+isDirection x = x == "R" || x == "L"
+
+toDirection :: String -> Direction
+toDirection "R" = R
+toDirection "L" = L
+toDirection _ = error "invalid direction in reading"
 
 --[Transition] == [(InTransition, OutTransition)] == [( (State,Alpha) , (State,TapeA,Char) )]
 canTransition :: [Transition] -> InTransition -> Transition
@@ -130,24 +121,69 @@ canTransition (t:ts) (s,a) | first (first t) == s && second (first t) == a = sec
 --returns new State, new TapePosition, newTape
 update :: state -> String -> Integer -> String -> (State, Integer, String)
 update s i tp t = let trans  = canTransition transitions (s,i)
-                      tpoint a | a == 'L' || a == 'l' = -1
-                               | a == 'R' || a == 'r' = 1
-                               | otherwise = 0
+                      tpoint a | a == L = -1
+                               | a == R = 1
                       nTape a b (x:xs) | a == 0 = (b:xs)
                                        | a > 0  = x:(ntape (a-1) b xs)
                   in ((first trans), (tpoint (third trans)), (nTape tp (second trans) t))
 
 currentPointer = 1;
-tape = [empty:empty:empty]
+-- pointer points to element in the tape. If the pointer is ever > or < the tape, add a blank element in front
+tape = inputTape
+-- take in current state, position of the pointer, tape, and input string
 turing :: State -> Integer -> [TapeA] -> String -> State
 turing -1 _ _ _ = -1
 turing currentState _ _ [] = currentState
-turing currentState currentPointer tape (x:xs)  | isValid x && currentState >= 0 =  let u = update currentState currentPointer tape
-                                                                                        a = first u
-                                                                                        b = second u
-                                                                                        c = currentPointer + (third u)
-                                                                                    in  turing (a b c xs)
-                                                | otherwise = -1
+turing s p t (x:xs) | isValid x && s >= 0 && p > 0 && p < (length t) =  let u = update s p t
+                                                                            a = first u
+                                                                            b = second u
+                                                                            c = currentPointer + (third u)
+                                                                        in  turing (a b c xs)
+                    | isValid x && s >= 0 && p == 0 = let u = update s 1 (blank:t)
+                                                          a = first u
+                                                          b = second u
+                                                          c = currentPointer + (third u)
+                                                      in  turing (a b c xs)
+                    | isValid x && s >= 0 && p > (length t) = let u = update s p (t++[blank])
+                                                                  a = first u
+                                                                  b = second u
+                                                                  c = currentPointer + (third u)
+                                                              in  turing (a b c xs)
+                    | otherwise = -1
+
+-- extracting from read/sr
+lexerParser :: String -> ([State], [TapeA], [TapeA], [Transition], State, TapeA, [State])
+lexerParser l = sr (lexer l)
+
+lexer :: String -> [Token]
+lexer s = map classify (words (addSpaces s))
+
+--
+-- May have to go in main
+machine :: ([State], [TapeA], [TapeA], [Transition], State, TapeA, [State])
+machine = -- output of lexerParser
+
+
+states :: [State]
+states = first machine
+
+tape :: [TapeA]
+tape = second machine
+
+alphabet :: [TapeA]
+alphabet = third machine
+
+transitions :: [Transition]
+transitions = fourth machine
+
+start :: State
+start = fifth machine
+
+blank :: TapeA
+blank = sixth machine
+
+final :: [State]
+final = seventh machine
 
 isFinal :: State -> Bool
 isFinal s = s `elem` states
